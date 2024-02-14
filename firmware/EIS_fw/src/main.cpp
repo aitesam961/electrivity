69e52fdcf16f86abc79b25cb6762bfac45cf8899
aitesam961 <aitesam961@gmail.com>
2024-02-14 10:30:54 +0500

    Update main.cpp

--- firmware/EIS_fw/src/main.cpp
+++ firmware/EIS_fw/src/main.cpp
@@ -7,7 +7,7 @@
 #define SERVICE_UUID        "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
 #define TEMPERATURE_UUID    "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 #define CONTROL_UUID        "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
-#define BLE_NAME            "ESP32-Node-1"
+#define BLE_NAME            "ESP32-Sensor-Node"
 
 BLEServer *pServer = NULL;
 BLECharacteristic *pCharacteristicTemperature = NULL;

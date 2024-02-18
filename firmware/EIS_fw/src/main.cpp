267f9e7da10ac73a942252671d691d6cb645ed78
aitesam961 <aitesam961@gmail.com>
2024-02-18 12:24:25 +0500

    Create new Firmware projectcfiles

--- firmware/EIS_fw/src/main.cpp
+++
@@ -1,1 +0,0 @@
-// Paste the respective code from secondry source files
---
+++ firmware/eis_esp_mqtt_comm/src/main.cpp
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";

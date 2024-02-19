c7cf562b7c6ac34816ea731d9d4b342080aa6136
aitesam961 <aitesam961@gmail.com>
2024-02-19 17:02:53 +0500

    Update main.cpp

--- firmware/eis_esp_mqtt_comm/src/main.cpp
+++ firmware/eis_esp_mqtt_comm/src/main.cpp
@@ -1,7 +1,81 @@
-#include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-// Network credentials
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "M.INAM UL HAQ";
+const char* password = "Easy@13290.Wifi";
+const char* mqttServer = "192.168.8.102";
+const int mqttPort = 1883;
+const char* mqttClientID = "ESP32Client";
+const char* switchTopic = "esp32/switch";
+const char* ledTopic = "esp32/led";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const int switchPin = 4;
+const int ledPin = 2;
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  if (String(topic) == ledTopic) {
+    if (message == "on") {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("LED turned ON");
+    } else if (message == "off") {
+      digitalWrite(ledPin, LOW);
+      Serial.println("LED turned OFF");
+    }
+  }
+}
+
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(mqttClientID)) {
+      Serial.println("connected");
+      client.subscribe(ledTopic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(switchPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+}
+
+
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  int switchState = digitalRead(switchPin);
+  if (switchState == HIGH) {
+    client.publish(switchTopic, "high");
+    Serial.println("Switch HIGH");
+  }
+}
